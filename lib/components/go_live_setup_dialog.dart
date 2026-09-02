179c4479ecadb83c5a48054f882536257c110f49
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-09-02 19:46:58 +0100

    fix(mobile): fix _startLive method syntax in go_live_setup_dialog

--- lib/components/go_live_setup_dialog.dart
+++ lib/components/go_live_setup_dialog.dart
@@ -3,7 +3,6 @@ import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../core/api_client.dart';
-import '../core/permissions_service.dart';
 import '../models/community_models.dart';
 import '../screens/live_stream_screen.dart';
 
