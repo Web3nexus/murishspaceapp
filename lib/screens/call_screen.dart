a078c16e22352c4f0f3356bd81e6df1eb294d437
Vincent <125793121+Web3nexus@users.noreply.github.com>
2026-09-16 10:13:30 +0100

    fix(calling): hide ConnectionState from flutter/material.dart to resolve ambiguity with livekit_client

--- lib/screens/call_screen.dart
+++ lib/screens/call_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide ConnectionState;
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:livekit_client/livekit_client.dart';
